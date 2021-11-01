04320e000ef39a7b93ba0c7b66053a3e4f3c6254
Anar Seyf <anar.seyf@gmail.com>
2021-10-31 19:11:31 -0700

    completed

--- test2.r
+++
@@ -1,9 +0,0 @@
-install.packages("Tmisc", repos = "http://cran.us.r-project.org")
-library(Tmisc)
-data(quartet)
-
-head(quartet, n = 20)
-
-quartet %>%
-  group_by(set) %>%
-  summarise(mean(x), sd(x), mean(y), sd(y), cor(x, y))
